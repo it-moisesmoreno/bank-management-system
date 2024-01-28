0981088ea686076328a394ea171787c5e2a95bc1
Moises Moreno <it.moisesmoreno@gmail.com>
2024-01-27 18:24:46 -0600

    Update label text size

--- bank_management_system/src/main/java/com/example/Login.java
+++ bank_management_system/src/main/java/com/example/Login.java
@@ -14,20 +14,20 @@ public class Login extends JFrame{
 
         setLayout(null);
 
-        /*ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("logo.png"));
+        ImageIcon imageIcon = new ImageIcon(ClassLoader.getSystemResource("logo.png"));
         Image image = imageIcon.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon customIcon = new ImageIcon(image);
         JLabel label = new JLabel(customIcon);
         label.setBounds(70,10,100,100);
-        add(label);*/
+        add(label);
 
         JLabel welcomeText = new JLabel( "Welcome to ATM");
-        welcomeText.setBounds(200, 40, 10, 40);
+        welcomeText.setBounds(200, 40, 100, 40);
         add(welcomeText);
 
         getContentPane().setBackground(Color.WHITE);
 
-        setSize(400, 400);
+        setSize(500, 400);
         setVisible(true);
         setLocation(350,200);
     }
